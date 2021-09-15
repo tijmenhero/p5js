94ec2cce0039002f7f6393c6f647de5145eb4be4
Tijmen Schouten <74767479+tijmenhero@users.noreply.github.com>
2021-09-15 10:50:53 +0000

    makkelijk balletjhes teklenen

--- script.js
+++ script.js
@@ -1,60 +1,49 @@
-var x, y, m, n, v;
 
-function setup() {
-   createCanvas(500, 400)
 
-   x = 0;
-   y = 200;
-   m = 200;
-   n = 0;
-   vx = 5;
-   vy = 5;
-   vm = 2;
-   vn = 2;
-}
+class Ball {
 
-function draw() {
-  background(225);
-  let c = color('red')
-  let o = color('green')
-
-  ellipse(x, y,50,50,);
-  fill(c)
-  x = x + vx
-  y = y + vy
-
-  if(x < 0 || x > 500){
-    vx = vx * -1
-  }
-
-  if(y < 0 || y > 400)
-   vy = vy * -1
-
-   ellipse(m, n,50,50,);
-  fill(o)
-  m = m + vm
-  n = n + vn
-
-  if(m < 0 || m > 500){
-    vm = vm * -1
-  }
-
-  if(n < 0 || n > 400)
-   vn = vn * -1
-}
-
-class ball {
-
-  constructor(x, y, w, h, vx, vy) {
+  constructor(x, y, w, h, vx, vy,) {
     this.x = x
     this.y = y
     this.w = w
     this.h = h
     this.vx = vx
     this.vy = vy
+    this.color = color
   }
 
-drawBall() {
-  ellipse(this.x, this.y, this.w, this.h)
+  drawBall() {
+    ellipse(this.x, this.y, this.w, this.h)
+    this.x = this.x + this.vx
+    this.y = this.y + this.vy
+
+    if(this.x < 0 || this.x > 500){
+      this.vx = this.vx * -1
+    }
+
+    if(this.y < 0 || this.y > 400){
+    this.vy = this.vy * -1
+    }
+  }
 }
+
+function setup() {
+  createCanvas(500, 400)
+
+  ball1 = new Ball(0, 300, 50, 50,5,5)
+  ball2 = new Ball(200, 100, 40, 40,3,3)
+  ball3 = new Ball(50, 200, 60, 60,6,6)
+  ball4 = new Ball(260, 360, 20, 20,2,2)
+  ball5 = new Ball(500, 400, 30, 30,6,6)
 }
+
+function draw() {
+  background(225);
+
+  ball1.drawBall()
+  ball2.drawBall()
+  ball3.drawBall()
+  ball4.drawBall()
+  ball5.drawBall()
+}
+  
