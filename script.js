788485aa7dad8c418b6bfae2b2df9e30a7f87fbf
Tijmen Schouten <74767479+tijmenhero@users.noreply.github.com>
2021-09-15 14:46:08 +0000

    ook vierkanten

--- script.js
+++ script.js
@@ -2,18 +2,32 @@
 
 class Ball {
 
-  constructor(x, y, w, h, vx, vy,) {
-    this.x = x
-    this.y = y
-    this.w = w
-    this.h = h
-    this.vx = vx
-    this.vy = vy
-    this.color = color
+  constructor(x, y, w, h, vx, vy, color) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.vx = vx;
+    this.vy = vy;
+    this.color = color;
   }
 
   drawBall() {
-    ellipse(this.x, this.y, this.w, this.h)
+    ellipse(this.x, this.y, this.w, this.h, this.color)
+    this.x = this.x + this.vx
+    this.y = this.y + this.vy
+
+    if(this.x < 0 || this.x > 500){
+      this.vx = this.vx * -1
+    }
+
+    if(this.y < 0 || this.y > 400){
+    this.vy = this.vy * -1
+    }
+  }
+
+  drawRect() {
+    rect(this.x, this.y, this.w, this.h, this.color)
     this.x = this.x + this.vx
     this.y = this.y + this.vy
 
@@ -30,11 +44,13 @@ class Ball {
 function setup() {
   createCanvas(500, 400)
 
-  ball1 = new Ball(0, 300, 50, 50,5,5)
+  ball1 = new Ball(0, 300, 50, 50, 5, 5, 'red')
   ball2 = new Ball(200, 100, 40, 40,3,3)
   ball3 = new Ball(50, 200, 60, 60,6,6)
   ball4 = new Ball(260, 360, 20, 20,2,2)
   ball5 = new Ball(500, 400, 30, 30,6,6)
+  rect1 = new Ball(400, 50, 70, 70,3,3)
+  rect2 = new Ball(500, 0, 30, 30,6,6)
 }
 
 function draw() {
@@ -45,5 +61,7 @@ function draw() {
   ball3.drawBall()
   ball4.drawBall()
   ball5.drawBall()
+  rect1.drawRect()
+  rect2.drawRect()
 }
   
